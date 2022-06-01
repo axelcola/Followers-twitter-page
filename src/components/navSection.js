4950e528096f811df1c2c7adac13c44df8244d2c
Axel Cola <axelcola1@gmail.com>
2022-06-01 15:58:57 -0300

    boxes to hover

--- src/components/navSection.js
+++ src/components/navSection.js
@@ -10,57 +10,78 @@ import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
 import "./navSection.css";
 const NavSection = () => {
   return (
-    <div className="section">
+    <div className="nav-section">
       <div className="logo">
-        <BsTwitter size={30} />
+        <div className="icon">
+          <BsTwitter size={30} />
+        </div>
       </div>
       <div className="nav-button home">
         <div className="icon">
           <GoHome size={23} />
         </div>
+        <div className="box"></div>
         <h1 className="button-text">Home</h1>
       </div>
       <div className="nav-button explore">
         <div className="icon">
           <FiHash size={23} />
         </div>
+        <div className="box"></div>
         <h1 className="button-text">Explore</h1>
       </div>
       <div className="nav-button notifications">
         <div className="icon">
           <RiNotification2Line size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">Notifications</h1>
       </div>
       <div className="nav-button messages">
         <div className="icon">
           <HiOutlineMail size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">Messages</h1>
       </div>
       <div className="nav-button bookmarks">
         <div className="icon">
           <FiBookmark size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">Bookmarks</h1>
       </div>
       <div className="nav-button lists">
         <div className="icon">
           <BsCardText size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">Lists</h1>
       </div>
       <div className="nav-button profile">
         <div className="icon">
           <AiOutlineUser size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">Profile</h1>
       </div>
-      <div className="nav-button more">
+      <div className="nav-button profile">
         <div className="icon">
           <HiOutlineDotsCircleHorizontal size={23} />
         </div>
+        <div className="box"></div>
+
         <h1 className="button-text">More</h1>
+      </div>
+      <div>
+        <div className="new-tweet-button">
+          <h1>Tweet</h1>
+        </div>
       </div>
     </div>
   );
