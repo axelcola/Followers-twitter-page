8646fb951d5a3b5da4fda7f422bae6043ce07cb7
Axel Cola <axelcola1@gmail.com>
2022-06-02 21:43:24 -0300

    modal works, falta hacerle fade in y un delay

--- src/components/userCard.js
+++ src/components/userCard.js
@@ -1,16 +1,35 @@
 import img from "./IMG_2441.jpeg";
 import "./userCard.css";
 import { BsThreeDots } from "react-icons/bs";
+import { useState, renderLikeList, handleLeave, handleHover } from "react";
 
 export default function UserCard() {
+  const [state, setState] = useState({ likeList: "" });
+
+  renderLikeList = () => {
+    return <div className="pop-user">Likes to be rendered specifically</div>;
+  };
+  handleLeave = () => {
+    return setState({ likeList: "" });
+  };
+  handleHover = () => {
+    console.log("hola");
+    return setState({ likeList: renderLikeList() });
+  };
+
   return (
     <>
       <div className="user-card">
         <div className="img-and-data">
           <img src={img} alt="user-img" className="user-photo"></img>
           <div className="data">
-            <h1 className="user-name">Axel</h1>
-            <div className="pop-user"></div>
+            <h1
+              className="user-name"
+              onMouseOver={handleHover}
+              onMouseLeave={handleLeave}
+            >
+              Axel {state.likeList}
+            </h1>
             <p>@axel</p>
             <h2>descripción sobre lo efimero de la vida</h2>
           </div>
