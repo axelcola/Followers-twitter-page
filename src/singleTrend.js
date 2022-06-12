73441ba460b00aa0f7347f982a40bf9549de8f97
Axel Cola <axelcola1@gmail.com>
2022-06-12 14:57:53 -0300

    trends

--- src/singleTrend.js
+++ src/singleTrend.js
@@ -1,22 +1,22 @@
 import "./signleTrend.css";
 import { BsThreeDots } from "react-icons/bs";
 
-export default function SingleTrend() {
+export default function SingleTrend(props) {
   return (
     <div className="single-trend">
       <div className="trend-first-div">
         <div className="trend-category">
-          <h1>Trending in uruguay</h1>
+          <h1>{props.category}</h1>
         </div>
         <div className="trend-dots">
           <BsThreeDots size={16} />
         </div>
       </div>
       <div className="trend-name">
-        <h3>Uruguay</h3>
+        <h3>{props.name}</h3>
       </div>
       <div className="number-tweets">
-        <p>15.32K Tweets</p>
+        <p>{props.tweets}</p>
       </div>
     </div>
   );
--- src/trendsBox.js
+++ src/trendsBox.js
@@ -1,6 +1,58 @@
 import SingleTrend from "./singleTrend";
 import "./trendsBox.css";
 const TrendBox = () => {
+  const trends = [
+    {
+      name: "Valverde",
+      tweets: "9,320",
+      category: "Sports",
+    },
+    {
+      name: "Dua lipa",
+      tweets: "35.6K",
+      category: "Music",
+    },
+    {
+      name: "ugarte",
+      tweets: "",
+      category: "Sports",
+    },
+    {
+      name: "Montevideo",
+      tweets: "4,320",
+      category: "Trending in Uruguay",
+    },
+    {
+      name: "Muslera",
+      tweets: "1,253",
+      category: "Sports",
+    },
+    {
+      name: "Suárez",
+      tweets: "24.56K",
+      category: "Sports",
+    },
+    {
+      name: "Centenario",
+      tweets: "4,256",
+      category: "Trending in Uruguay",
+    },
+    {
+      name: "Cami",
+      tweets: "16,7K",
+      category: "Trending in Uruguay",
+    },
+    {
+      name: "Santi",
+      tweets: "29.8K",
+      category: "Trending in Uruguay",
+    },
+    {
+      name: "Suiza",
+      tweets: "5,176",
+      category: "Sports",
+    },
+  ];
   return (
     <>
       <div>
@@ -8,14 +60,13 @@ const TrendBox = () => {
           <div className="title-box">
             <h1 className="trend-title">Trends for you</h1>
           </div>
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
-          <SingleTrend />
+          {trends.map((trend) => (
+            <SingleTrend
+              name={trend.name}
+              category={trend.category}
+              tweets={trend.tweets}
+            />
+          ))}
           <div className="showmore-trend">
             <h3>Show more</h3>
           </div>
